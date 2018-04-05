23461f14c68503dd5d1ef15a6cf73c34fdb2afdd
Italo Morales F <i@italomoralesf.com>
2018-04-05 15:51:53 -0500

    Tutorial con spatie/laravel-permission

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>403</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+                margin-top: 60px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="content">
+            <div class="title">
+                Error 403
+            </div>
+        </div>
+    </body>
+</html>
